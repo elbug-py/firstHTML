59f242447eed9dc7203fa0c6bf76586a7f5b1e8e
Vicente Romero <73198039+elbug-py@users.noreply.github.com>
2022-01-04 15:15:51 -0300

    Delete main.js

--- Desktop/javascript_mine/image_app/project02-elbug-py/scripts/main.js
+++
@@ -1,8 +0,0 @@
-
-const im = require('./image_modifier.js');
-
-const IMAGE_FILE_NAME = 'yui.jpeg';
-
-im.imageToGreyscale(IMAGE_FILE_NAME, IMAGE_FILE_NAME);
-im.imageToNegative(IMAGE_FILE_NAME, IMAGE_FILE_NAME);
-im.imageToSepia(IMAGE_FILE_NAME, IMAGE_FILE_NAME);
